02ef6c7eab18c9578980d3d7db3e09a5e2386cf2
Eric McCormick <Eric McCormick>
2017-02-21 09:56:49 -0500

    buttons added

---
+++ initialJS.js
@@ -0,0 +1,9 @@
+var app = angular.module('myModule', []);
+
+app.controller('myController', function($scope){
+
+$scope.words = ['jazz', 'music', 'edm', 'maybeMusic', 'pop', 'rock', 'moreJazz'];
+
+
+
+});
