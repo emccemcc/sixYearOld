e95e58e891f6c38704ae74cbf43bd55aaf9c836f
Eric McCormick <Eric McCormick>
2017-02-21 10:15:33 -0500

    most recent

--- initialJS.js
+++ initialJS.js
@@ -1,9 +1,26 @@
 var app = angular.module('myModule', []);
 
 app.controller('myController', function($scope){
+//displayed array
+$scope.btnItemsShown = [];
 
+//prepopulated array?
 $scope.words = ['jazz', 'music', 'edm', 'maybeMusic', 'pop', 'rock', 'moreJazz'];
 
+
+//for selection - currently not working
+$scope.selected = null;
+$scope.select = function (index){
+  $scope.selected = index;
+};
+
+$scope.clicked = function() {
+    if ($scope.words) {
+      $scope.toDoItems.push({taskName:$scope.masterList.taskName});
+      $scope.masterList = '';
+        //will clear out items
+    }
+  };
 
 
 });
